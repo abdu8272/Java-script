777fe5624e6a07dda1f62907a8ac9814708ed5ae
abdu <abdu>
2024-05-19 13:54:01 +0500

    Js

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+var name = prompt("Ismingizni kiriting:")
+
+console.log("Sizning ismingiz: " + name)
+
+var age = prompt("Yoshingizni kiriting")
+console.log("Sizning yoshingiz: " + age)
+//+
+var misol1 = + prompt("Misolni yeching:5+4=?")
+console.log(" Misol1: " + '5 + 4 = 9' + " Sizning jovobingiz: " + misol1)
+
+var misol2 = + prompt("Misolni yeching:11+4=?")
+console.log(" Misol2: " + '11 + 4 =15' + " Sizning jovobingiz: " + misol2)
+//-
+var misol3 = + prompt("Misolni yeching:19-8=?")
+console.log(" Misol3: " + '19 - 8 = 1' + " Sizning jovobingiz: " + misol3)
+
+var misol4 = + prompt("Misolni yeching:26-12=?")
+console.log(" Misol4: " + '26 - 12 = 14' + " Sizning jovobingiz: " + misol4)
+
+//*
+var misol5 = + prompt("Misolni yeching:8*7=?")
+console.log(" Misol5: " + '8 * 7 = 56' + " Sizning jovobingiz: " + misol5)
+
+var misol6 = + prompt("Misolni yeching:5*5=?")
+console.log(" Misol6: " + '5 * 5 = 25' + " Sizning jovobingiz: " + misol6)
+//./
+var misol7 = + prompt("Misolni yeching:20/4=?")
+console.log(" Misol7: " + ' 20 / 4 = 5' + " Sizning jovobingiz: " + misol7)
+
+var misol8 = + prompt("Misolni yeching:21/3=?")
+console.log(" Misol8: " + ' 21 / 3 = 7' + " Sizning jovobingiz: " + misol8)
+//%
+var misol9 = + prompt("Misolni yeching:25%4=?")
+console.log(" Misol9: " + ' 25 % 4 = 1' + " Sizning jovobingiz: " + misol9)
+
+var misol10 = + prompt("Misolni yeching:29%3=?")
+console.log(" Misol10: " + ' 29 % 3 = 2' + " Sizning jovobingiz: " + misol10)
+
+prompt("Natijani ko'rish uchun konsolni oching")
+
+
